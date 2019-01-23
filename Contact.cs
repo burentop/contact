40c7673866b4910371f54c4df6fb145e207a79b8
Brent Perry <contact@brentallan.me>
2019-01-22 18:24:19 -0600

    initialize repo

---
+++ Contact.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+class Contact
+{
+    public string Name;
+    public string Phone;
+    public string Address;
+}
+
+class Program
+{
+    public static void Main()
+    {
+        
+    }
+}
